736a21e72582c406b608c3e1a70350e5cdc720aa
Narendran <narendev0610@gmail.com>
2020-09-13 18:35:32 +0530

    mobile, desktop responsiveness added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,70 +6,179 @@ import '../data/data.dart';
 import '../models/models.dart';
 import '../widgets/widgets.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final TrackingScrollController _trackingScrollController =
+      TrackingScrollController();
+
+  @override
+  void dispose() {
+    _trackingScrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-          child: Scaffold(
-        body: CustomScrollView(
-          slivers: [
-            SliverAppBar(
-              brightness: Brightness.light,
-              backgroundColor: Colors.white,
-              title: Text(
-                'facebook',
-                style: const TextStyle(
-                  color: Palette.facebookBlue,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: -1.2,
-                ),
-              ),
-              centerTitle: false,
-              floating: true,
-              actions: [
-                CircleButton(
-                  icon: Icons.search,
-                  iconSize: 30.0,
-                  onPressed: () => print('Search'),
-                ),
-                CircleButton(
-                  icon: MdiIcons.facebookMessenger,
-                  iconSize: 30.0,
-                  onPressed: () => print('Messenger'),
-                ),
-              ],
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        body: Responsive(
+          mobile:
+              _HomeScreenMobile(scrollController: _trackingScrollController),
+          desktop:
+              _HomeScreenDesktop(scrollController: _trackingScrollController),
+        ),
+      ),
+    );
+  }
+}
+
+class _HomeScreenMobile extends StatelessWidget {
+  final TrackingScrollController scrollController;
+
+  const _HomeScreenMobile({
+    Key key,
+    @required this.scrollController,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      controller: scrollController,
+      slivers: [
+        SliverAppBar(
+          brightness: Brightness.light,
+          backgroundColor: Colors.white,
+          title: Text(
+            'facebook',
+            style: const TextStyle(
+              color: Palette.facebookBlue,
+              fontSize: 28.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: -1.2,
             ),
-            SliverToBoxAdapter(
-              child: CreatePostContainer(currentUser: currentUser),
+          ),
+          centerTitle: false,
+          floating: true,
+          actions: [
+            CircleButton(
+              icon: Icons.search,
+              iconSize: 30.0,
+              onPressed: () => print('Search'),
             ),
-            SliverPadding(
-              padding: const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 5.0),
-              sliver: SliverToBoxAdapter(
-                child: Rooms(onlineUsers: activeUsers),
-              ),
-            ),
-            SliverPadding(
-              padding: const EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 5.0),
-              sliver: SliverToBoxAdapter(
-                child: Stories(
-                  currentUser: currentUser,
-                  stories: stories,
-                ),
-              ),
-            ),
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-                (context, index) {
-                  final Post post = posts[index];
-                  return PostContainer(post: post);
-                },
-                childCount: posts.length,
-              ),
+            CircleButton(
+              icon: MdiIcons.facebookMessenger,
+              iconSize: 30.0,
+              onPressed: () => print('Messenger'),
             ),
           ],
         ),
-      ),
+        SliverToBoxAdapter(
+          child: CreatePostContainer(currentUser: currentUser),
+        ),
+        SliverPadding(
+          padding: const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 5.0),
+          sliver: SliverToBoxAdapter(
+            child: Rooms(onlineUsers: activeUsers),
+          ),
+        ),
+        SliverPadding(
+          padding: const EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 5.0),
+          sliver: SliverToBoxAdapter(
+            child: Stories(
+              currentUser: currentUser,
+              stories: stories,
+            ),
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              final Post post = posts[index];
+              return PostContainer(post: post);
+            },
+            childCount: posts.length,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _HomeScreenDesktop extends StatelessWidget {
+  final TrackingScrollController scrollController;
+
+  const _HomeScreenDesktop({
+    Key key,
+    @required this.scrollController,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Flexible(
+          flex: 2,
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: MoreOptionsList(currentUser: currentUser),
+            ),
+          ),
+        ),
+        const Spacer(),
+        Container(
+          width: 600.0,
+          child: CustomScrollView(
+            controller: scrollController,
+            slivers: [
+              SliverPadding(
+                padding: const EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 10.0),
+                sliver: SliverToBoxAdapter(
+                  child: Stories(
+                    currentUser: currentUser,
+                    stories: stories,
+                  ),
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: CreatePostContainer(currentUser: currentUser),
+              ),
+              SliverPadding(
+                padding: const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 5.0),
+                sliver: SliverToBoxAdapter(
+                  child: Rooms(onlineUsers: activeUsers),
+                ),
+              ),
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    final Post post = posts[index];
+                    return PostContainer(post: post);
+                  },
+                  childCount: posts.length,
+                ),
+              ),
+            ],
+          ),
+        ),
+        const Spacer(),
+        Flexible(
+          flex: 2,
+          child: Align(
+            alignment: Alignment.centerRight,
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: ContactsList(users: activeUsers),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
